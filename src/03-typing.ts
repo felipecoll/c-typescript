b422db1e4f792e5489c13a39749273a2706a99bb
Felipe Coll <leonfelipecoll@gmail.com>
2025-02-07 14:54:19 -0300

    03 - Typing

---
+++ src/03-typing.ts
@@ -0,0 +1,13 @@
+(() => {
+  myProductName = 'cambiar valor'
+  myProductName.toLowerCase()
+
+  myProductPrice.toFixed()
+
+  const myProductStock = 1000
+  const myProductStock2 = 'producto 2'
+})()
+
+
+
+
