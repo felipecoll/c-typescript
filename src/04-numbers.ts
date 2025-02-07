79f72a316fb4fa7e55c12b2d8b148777b9a2af0a
Felipe Coll <leonfelipecoll@gmail.com>
2025-02-07 15:47:34 -0300

    numbers y boolean

---
+++ src/04-numbers.ts
@@ -0,0 +1,17 @@
+(()=> {
+  let productPrice = 100
+  productPrice = 105.50
+  console.log('productPrice', productPrice)
+
+  let customerAge: number = 25
+  customerAge = customerAge + 1 // 26
+  console.log('customerAge', customerAge)
+
+  let productInStock: number
+  //console.log('productInStock', productInStock) // undefined
+
+if(productInStock > 10) {
+  console.log('Is greater')
+}
+
+})()
