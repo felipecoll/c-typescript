142764f93f07bf0d996d2f282ee2e71a9b0ae8cf
Felipe Coll <leonfelipecoll@gmail.com>
2025-02-11 16:22:38 -0300

    products example 2

---
+++ src/products/main.ts
@@ -0,0 +1,19 @@
+import { addProduct, calcStock, products } from "./product.service";
+
+addProduct ({
+  title: 'Pro1',
+  createdAt: new Date(1993, 1,1),
+  stock: 5
+})
+
+addProduct ({
+  title: 'Pro1',
+  createdAt: new Date(1993, 1,1),
+  stock: 6,
+  size: 'XL'
+})
+
+console.log(products)
+const total = calcStock()
+console.log(total)
+
--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -1,7 +1,15 @@
 import { Product } from './product.model'
 
-const products : Product[] = []
+export const products : Product[] = []
 
-const addProduct = (data: Product) => {
+export const addProduct = (data: Product) => {
   products.push(data)
+}
+
+export const calcStock = () : number => {
+  let total = 0
+  products.forEach(item => {
+    total =+ item.stock
+  })
+  return total
 }
